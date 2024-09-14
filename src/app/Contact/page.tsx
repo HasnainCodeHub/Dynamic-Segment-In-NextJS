999d28f42425a6405a557dc92c4fad89ffebf7e5
Hasnain Ali <hasnainbusiness204@gmail.com>
2024-09-15 01:20:08 +0500

    page.tsx

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -9,7 +9,7 @@ function contact() {
             <h1 className="text-3xl text-center text-black ml-48 mt-24 mr-48">Phone:+923702537927 </h1>
             <h1 className="text-3xl text-center text-black ml-48  ">Email:azeemhusnain048@gmail.com </h1>
             <h1 className="text-3xl text-center text-black ml-40 mr-44 ">Instagram:Has_Nain_333 </h1>
-            <button className="bg-red-600 text-white ml-12 mt-24 h-12"><Link href='http://localhost:3000/'>Back To Home Page</Link></button>
+            <button className="bg-red-600 text-white ml-12 mt-24 h-12"><Link href='/'>Back To Home Page</Link></button>
 
         </main>
     )
